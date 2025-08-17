34e40da27dcdecd2828c4c501fae30f59db58a55
louhabali <louhabali@gmail.com>
2025-08-17 10:09:55 +0100

    2nd version

--- geometrical_shapes/Circle.java
+++ geometrical_shapes/Circle.java
@@ -19,36 +19,40 @@ public class Circle implements Drawable {
 
     public static Circle random(int maxX, int maxY) {
         Point p = Point.random(maxX, maxY);
-        int r = (int)(Math.random() * Math.min(maxX, maxY) / 10);
+        int r = (int)(Math.random() * Math.min(maxX, maxY));
         return new Circle(p, r);
+    }
+    private double calculateDistance(double x1, double y1, double x2, double y2) {
+        double dx = x2 - x1;
+        double dy = y2 - y1;
+        return Math.sqrt(dx * dx + dy * dy);
     }
 
     @Override
-    public void draw(Displayable displayable) {
-        int x0 = center.x;
-        int y0 = center.y;
-        int x = radius;
-        int y = 0;
-        int err = 0;
+    public void draw(Displayable img) {
+        int cx = center.x;
+        int cy = center.y;
+        double r = (double) radius;
+        int x = cx;
+        int y = (int) (cy - r);
+        while (y <= cy) {
+            double a = Math.abs(calculateDistance(cx, cy, x + 1, y) - r);
+            double b = Math.abs(calculateDistance(cx, cy, x, y + 1) - r);
+            double c = Math.abs(calculateDistance(cx, cy, x + 1, y + 1) - r);
+            double min = Math.min(a, Math.min(b, c));
 
-        while (x >= y) {
-            displayable.display(x0 + x, y0 + y, color);
-            displayable.display(x0 + y, y0 + x, color);
-            displayable.display(x0 - y, y0 + x, color);
-            displayable.display(x0 - x, y0 + y, color);
-            displayable.display(x0 - x, y0 - y, color);
-            displayable.display(x0 - y, y0 - x, color);
-            displayable.display(x0 + y, y0 - x, color);
-            displayable.display(x0 + x, y0 - y, color);
-
-            y++;
-            if (err <= 0) {
-                err += 2*y + 1;
+            if (a == min) {
+                x += 1;
+            } else if (b == min) {
+                y += 1;
+            } else if (c == min) {
+                x += 1;
+                y += 1;
             }
-            if (err > 0) {
-                x--;
-                err -= 2*x + 1;
-            }
+            img.display(x, y, color);
+            img.display(x, cy + (cy - y), color);
+            img.display(2 * cx - x, y, color);
+            img.display(2 * cx - x, cy + (cy - y), color);
         }
     }
 
